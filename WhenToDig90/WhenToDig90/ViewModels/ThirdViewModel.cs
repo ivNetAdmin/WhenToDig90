9053b35234cb9f09bebb49ba3d0b3ba014071128
ivNetAdmin <bp@ivnet.co.uk>
2016-06-03 10:22:11 +0100

    Create ThirdViewModel.cs
    Navigation Service

---
+++ WhenToDig90/WhenToDig90/ViewModels/ThirdViewModel.cs
@@ -0,0 +1,29 @@
+namespace WhenToDig90.ViewModels
+{
+    public class ThirdViewModel:ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        private string _parameterText;
+
+        public ThirdViewModel(INavigationService navigationService)
+        {
+            if (navigationService == null) throw new ArgumentNullException("navigationService");
+            _navigationService = navigationService;
+
+            NavigateCommand = new RelayCommand(() => { _navigationService.GoBack(); });
+        }
+
+        public string ParameterText
+        {
+            get { return _parameterText; }
+            set
+            {
+                if (_parameterText == value) return;
+                _parameterText = value;
+                RaisePropertyChanged(() => ParameterText);
+            }
+        }
+
+        public ICommand NavigateCommand { get; set; }
+    }
+}
