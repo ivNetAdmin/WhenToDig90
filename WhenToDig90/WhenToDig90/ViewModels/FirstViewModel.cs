5c832146415d972935be0aacd1eaf4e3360c783f
ivNetAdmin <bp@ivnet.co.uk>
2016-06-03 10:19:57 +0100

    Update and rename MainViewModel.cs to FirstViewModel.cs
    Navigation Service

---
+++ WhenToDig90/WhenToDig90/ViewModels/FirstViewModel.cs
@@ -0,0 +1,21 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+
+namespace WhenToDig90.ViewModels
+{
+    public class FirstViewModel : ViewModelBase
+    {
+    private readonly INavigationService _navigationService;
+    
+    public FirstViewModel(INavigationService navigationService)
+        {
+            if (navigationService == null) throw new ArgumentNullException("navigationService");
+            _navigationService = navigationService;
+
+            NavigationCommand = new RelayCommand(() => { _navigationService.NavigateTo(Locator.SecondPage); });
+        }
+
+        public ICommand NavigationCommand { get; set; } 
+    }
+}
