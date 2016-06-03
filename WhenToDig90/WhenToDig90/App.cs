8ac355185bbbf6535466d5a245ab2a7e50e677fb
ivNetAdmin <bp@ivnet.co.uk>
2016-06-03 09:31:55 +0100

    Update App.cs
    App.cs with ViewModelLocator added

--- WhenToDig90/WhenToDig90/App.cs
+++ WhenToDig90/WhenToDig90/App.cs
@@ -11,35 +11,32 @@ namespace WhenToDig90
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
+            MainPage = new NavigationPage(GetMainPage());
+        }
+        private static ViewModelLocator _locator;
+        public static ViewModelLocator Locator
+        {
+            get
             {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+                return _locator ?? (_locator = new ViewModelLocator());
+            }
         }
 
+        public static Page GetMainPage()
+        {
+            return new MyPage();
+        }
         protected override void OnStart()
         {
-            // Handle when your app starts
+            // Handle when your app starts  
         }
-
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
+            // Handle when your app sleeps  
         }
-
         protected override void OnResume()
         {
-            // Handle when your app resumes
+            // Handle when your app resumes  
         }
     }
 }
