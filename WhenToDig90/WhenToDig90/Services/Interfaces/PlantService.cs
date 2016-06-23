920f0adb3adc1de335fa3039429a21fc387c09e5
ivNetAdmin <bp@ivnet.co.uk>
2016-06-23 16:01:57 +0100

    Create PlantService.cs
    plant service

---
+++ WhenToDig90/WhenToDig90/Services/Interfaces/PlantService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhenToDig90.Data.Entities;
+
+namespace WhenToDig90.Services.Interfaces
+{
+    public interface IPlantService
+    {
+       // Task Add(string name);
+
+       // Task Delete(int id);
+
+       // void GetAll(Action<Task<List<Job>>, Exception> callback);
+       // void GetJobsByMonth(Action<Task<List<Job>>, Exception> callback, DateTime date);
+       // void Get(Action<Task<Job>, Exception> callback, int id);
+
+        Task<int> Save(int plantId, string name, string sow, string harvest, string notes);
+        
+    }
+}
