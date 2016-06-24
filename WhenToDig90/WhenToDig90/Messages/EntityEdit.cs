bdf9cfc4b980f7c66bea2ced35c348043bd90256
ivNetAdmin <bp@ivnet.co.uk>
2016-06-24 11:22:57 +0100

    Update EntityEdit.cs
    EntityEdit.ValueList

--- WhenToDig90/WhenToDig90/Messages/EntityEdit.cs
+++ WhenToDig90/WhenToDig90/Messages/EntityEdit.cs
@@ -9,15 +9,6 @@ namespace WhenToDig90.Messages
     public class EntityEdit<T> : IEntityEdit<T> where T : class
     {
         public int Value { get; set; }
-        //private string _message = "Entity Added";
-        //public string Message()
-        //{
-        //    return _message;
-        //}
-
-        //public void UpdateMessage(string message)
-        //{
-        //    _message = message;
-        //}
+        public string ValueList { get; set; }
     }
 }
